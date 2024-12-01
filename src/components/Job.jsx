5ea54e849cb86e0692d35009e7860670445dc9dc
CarlosEduJs <carloseduardogit@gmail.com>
2024-12-01 08:37:44 -0300

    adjusted accessibility

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,5 +1,4 @@
 import { useFilters } from "../context/FiltersContext";
-
 import { motion, AnimatePresence } from "framer-motion";
 
 export const Job = ({
@@ -17,10 +16,11 @@ export const Job = ({
   role,
 }) => {
   const { addFilter, filters } = useFilters();
+
   return (
     <AnimatePresence>
       <motion.div
-        key={company + position} 
+        key={company + position}
         initial={{ opacity: 0, x: -50, scale: 0.9 }}
         animate={{ opacity: 1, x: 0, scale: 1 }}
         exit={{ opacity: 0, scale: 0, y: 20 }}
@@ -43,28 +43,29 @@ export const Job = ({
           />
           <div className="flex flex-col gap-2 max-md:relative max-md:bottom-5">
             <div className="flex items-center gap-2">
-              <span className="text-primary-desaturated-dark-cyan font-bold text-xs max-md:text-sm">
+              <span aria-label="company" className="text-primary-desaturated-dark-cyan font-bold text-xs max-md:text-sm">
                 {company}
               </span>
               {newJob && (
-                <span className=" bg-primary-desaturated-dark-cyan text-[9px] max-md:text-xs rounded-xl text-white px-2 ">
+                <span aria-label="new job" className="bg-primary-desaturated-dark-cyan text-[9px] max-md:text-xs rounded-xl text-white px-2 font-semibold">
                   NEW!
                 </span>
               )}
               {featured && (
-                <span className="bg-neutral-very-dark-grayish-cyan text-[9px] max-md:text-xs rounded-xl text-white px-1">
+                <span aria-label="featured" className="bg-neutral-very-dark-grayish-cyan text-[9px] max-md:text-xs rounded-xl text-white px-1 font-semibold">
                   FEATURED
                 </span>
               )}
             </div>
-            <span
+            <button
               onClick={() => addFilter({ filterName: level })}
-              className="text-xl font-bold max-md:text-2xl transition-all duration-500 hover:text-primary-desaturated-dark-cyan  cursor-pointer w-fit"
+              aria-label={`filter by position: ${position}`}
+              className="text-xl font-bold max-md:text-2xl transition-all duration-500 hover:text-primary-desaturated-dark-cyan cursor-pointer w-fit bg-transparent border-none"
             >
               {position}
-            </span>
+            </button>
             <ul className="flex items-center gap-2">
-              <li className="text-neutral-400 text-xs max-md:text-sm ">
+              <li className="text-neutral-400 text-xs max-md:text-sm">
                 {postedAt}
               </li>
               <li className="text-neutral-400 text-xs max-md:text-sm list-disc list-inside">
@@ -77,52 +78,52 @@ export const Job = ({
           </div>
         </div>
         <div className="flex items-center gap-4 max-md:grid max-md:grid-cols-5 max-sm:grid-cols-3 max-md:w-fit max-md:gap-2">
-          <span
+          <button
             onClick={() => addFilter({ filterName: role })}
             className={`transition-all duration-300 ${
               filters.some((filter) => filter.filterName === role)
-                ? "hover:bg-neutral-light-grayish-cyan-filter  hover:text-primary-desaturated-dark-cyan  bg-primary-desaturated-dark-cyan text-white"
-                : "bg-neutral-light-grayish-cyan-filter  text-primary-desaturated-dark-cyan  hover:bg-primary-desaturated-dark-cyan hover:text-white"
-            }  text-xs max-md:text-sm  font-bold px-2 py-1 rounded-md cursor-pointer  w-fit`}
+                ? "hover:bg-neutral-light-grayish-cyan-filter hover:text-primary-desaturated-dark-cyan bg-primary-desaturated-dark-cyan text-white"
+                : "bg-neutral-light-grayish-cyan-filter text-primary-desaturated-dark-cyan hover:bg-primary-desaturated-dark-cyan hover:text-white"
+            } text-xs max-md:text-sm font-bold px-2 py-1 rounded-md cursor-pointer w-fit outline-primary-desaturated-dark-cyan border-none`}
           >
             {role}
-          </span>
-          <span
+          </button>
+          <button
             onClick={() => addFilter({ filterName: level })}
             className={`transition-all duration-300 ${
               filters.some((filter) => filter.filterName === level)
-                ? "hover:bg-neutral-light-grayish-cyan-filter  hover:text-primary-desaturated-dark-cyan  bg-primary-desaturated-dark-cyan text-white"
-                : "bg-neutral-light-grayish-cyan-filter  text-primary-desaturated-dark-cyan  hover:bg-primary-desaturated-dark-cyan hover:text-white"
-            }  text-xs max-md:text-sm  font-bold px-2 py-1 rounded-md cursor-pointer  w-fit`}
+                ? "hover:bg-neutral-light-grayish-cyan-filter hover:text-primary-desaturated-dark-cyan bg-primary-desaturated-dark-cyan text-white"
+                : "bg-neutral-light-grayish-cyan-filter text-primary-desaturated-dark-cyan hover:bg-primary-desaturated-dark-cyan hover:text-white"
+            } text-xs max-md:text-sm font-bold px-2 py-1 rounded-md cursor-pointer w-fit outline-primary-desaturated-dark-cyan border-none`}
           >
             {level}
-          </span>
+          </button>
 
           {tools.map((tool, index) => (
-            <span
+            <button
               key={index}
+              onClick={() => addFilter({ filterName: tool })}
               className={`transition-all duration-300 ${
                 filters.some((filter) => filter.filterName === tool)
-                  ? "hover:bg-neutral-light-grayish-cyan-filter  hover:text-primary-desaturated-dark-cyan  bg-primary-desaturated-dark-cyan text-white"
-                  : "bg-neutral-light-grayish-cyan-filter  text-primary-desaturated-dark-cyan  hover:bg-primary-desaturated-dark-cyan hover:text-white"
-              }  text-xs max-md:text-sm  font-bold px-2 py-1 rounded-md cursor-pointer  w-fit`}
-              onClick={() => addFilter({ filterName: tool })}
+                  ? "hover:bg-neutral-light-grayish-cyan-filter hover:text-primary-desaturated-dark-cyan bg-primary-desaturated-dark-cyan text-white"
+                  : "bg-neutral-light-grayish-cyan-filter text-primary-desaturated-dark-cyan hover:bg-primary-desaturated-dark-cyan hover:text-white"
+              } text-xs max-md:text-sm font-bold px-2 py-1 rounded-md cursor-pointer w-fit outline-primary-desaturated-dark-cyan border-none`}
             >
               {tool}
-            </span>
+            </button>
           ))}
           {languages.map((language, index) => (
-            <span
+            <button
               key={index}
+              onClick={() => addFilter({ filterName: language })}
               className={`transition-all duration-300 ${
                 filters.some((filter) => filter.filterName === language)
-                  ? "hover:bg-neutral-light-grayish-cyan-filter  hover:text-primary-desaturated-dark-cyan  bg-primary-desaturated-dark-cyan text-white"
-                  : "bg-neutral-light-grayish-cyan-filter  text-primary-desaturated-dark-cyan  hover:bg-primary-desaturated-dark-cyan hover:text-white"
-              }  text-xs max-md:text-sm  font-bold px-2 py-1 rounded-md cursor-pointer  w-fit`}
-              onClick={() => addFilter({ filterName: language })}
+                  ? "hover:bg-neutral-light-grayish-cyan-filter hover:text-primary-desaturated-dark-cyan bg-primary-desaturated-dark-cyan text-white"
+                  : "bg-neutral-light-grayish-cyan-filter text-primary-desaturated-dark-cyan hover:bg-primary-desaturated-dark-cyan hover:text-white"
+              } text-xs max-md:text-sm font-bold px-2 py-1 rounded-md cursor-pointer w-fit outline-primary-desaturated-dark-cyan border-none`}
             >
               {language}
-            </span>
+            </button>
           ))}
         </div>
       </motion.div>
